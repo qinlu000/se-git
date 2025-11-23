c711e95f6e11a1f2dc177e916d5387e709917009
CS-minions <17330004bc@gmail.com>
2025-11-23 14:17:34 +0800

    Add multiply function

--- main.cpp
+++ main.cpp
@@ -4,7 +4,12 @@ void greet() {
     std::cout << "Hello from greet function!" << std::endl;
 }
 
+int multiply(int a, int b) {
+    return a * b;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "2 * 3 = " << multiply(2, 3) << std::endl;
     return 0;
 }
