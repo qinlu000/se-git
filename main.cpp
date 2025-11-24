d9ec47a49a5fd90caac666e0fbb7512aea98611b
tjj <3442859534@qq.com>
2025-11-24 13:36:46 +0800

    fix: 命名空间

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-
+using namespace std;
 void greet() {
-    std::cout << "Hello from greet function!" << std::endl;
+    cout << "Hello from greet function!" << endl;
 }
 
 int multiply(int a, int b) {
@@ -9,7 +9,7 @@ int multiply(int a, int b) {
 }
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "2 * 3 = " << multiply(2, 3) << std::endl;
+    cout << "Hello, World!" << endl;
+    cout << "2 * 3 = " << multiply(2, 3) << endl;
     return 0;
 }
